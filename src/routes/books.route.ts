5697ff15d26ea2cfc2281f1c518b6f386ba28e52
MarciaOrozco <orozcomarcia3@gmail.com>
2025-01-26 09:45:34 -0300

    ver detalles de un libro de manera publica

--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { getBook, getBooks, getBooksByCategory } from "../controllers";
-import { authMiddleware } from "../middlewares/auth.middlewares";
 
 const router = Router();
 
 router.get("/", getBooks);
-router.get("/:id", authMiddleware, getBook);
+router.get("/:id", getBook);
 router.get("/category/:id", getBooksByCategory);
 
 export default router;
