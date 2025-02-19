0d59b9931021a7478f4d8bba1b53787d55fa4489
MarciaOrozco <orozcomarcia3@gmail.com>
2025-02-19 09:21:14 -0300

    agrego carpeta api

---
+++ src/api/index.ts
@@ -0,0 +1,45 @@
+import express, { json, Request, Response } from "express";
+import router from "../routes";
+import connectDB from "../database";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+const allowedOrigins = [process.env.FRONT_END_URL];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: false,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+app.use(json());
+
+app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running.");
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+export default app;
